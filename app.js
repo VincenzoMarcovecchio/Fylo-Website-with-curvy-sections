8bb2e458c95e078244425481d2bc24df025c7d46
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2020-02-09 11:07:30 +0100

    email submit functionality added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("hello");
+const email = document.getElementsByName("email");
+const error = document.querySelector(".email-error");
+
+function validateInput() {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email[0].value.length < 1 || !email[0].value.match(mailformat)) {
+    email[0].style.border = "2px solid hsl(354, 100%, 66%)";
+    error.style.display = "block";
+  } else {
+    email[0].style.border = "1px solid hsl(223, 100%, 88%)";
+    error.style.display = "none";
+  }
+}
